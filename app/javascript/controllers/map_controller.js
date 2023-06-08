14a51d8aaa63906a9ce5e35f41b4f1c0ce7f2565
marc messager <marc.messager@gmail.com>
2023-06-08 17:51:55 -0400

    Google Maps code broken in maps_controller.js

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -8,55 +8,61 @@ export default class extends Controller {
   }
 
   connect() {
+    console.log(this.contentTarget)
     this.initMap()
   }
 
   async initMap() {
   // async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+
     const { Geocoder } = await google.maps.importLibrary("geocoding")
-    const {PinElement} = await google.maps.importLibrary("marker")
 
 
-    const beachFlagImg = document.createElement("img");
+
+    const beachFlagImg = document.createElement("img")
 
     // START - Icon - In case the marker is an icon
     beachFlagImg.src = "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
     // END - Icon
 
     // START - PinElement - In case the marker is a pin
+
+    // END - PinElement
+
+    // Request needed libraries.
+    const geo = new Geocoder
+    geo.geocode({ address: this.addressValue }, result => this.setMap(result))
+  }
+
+  async setMap(result) {
+    const { Map } = await google.maps.importLibrary("maps")
+    const { PinElement } = await google.maps.importLibrary("marker")
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker")
+
+    const position = result[0].geometry.location
+
+    const map = new Map(this.contentTarget, {
+      zoom: 15,
+      center: position,
+      mapId: "DEMO_MAP_ID",
+    });
+
     const mAptPin = new PinElement({
       borderColor: "blue",
       glyphColor: "black",
       background: "pink",
       glyph: "xyz",
       scale: 1.5,
+    })
+
+    // The marker
+    const marker = new AdvancedMarkerElement({
+      map: map,
+      position: position,
+      title: result[0].formatted_address,
+      content: mAptPin.element, // the marker is a pin
+      // content: beachFlagImg, // OR the marker is an icon
     });
-    // END - PinElement
-
-    const set_map = (result) => {
-      const position = result[0].geometry.location
-
-      const map = new Map(this.contentTarget, {
-        zoom: 15,
-        center: position,
-        mapId: "DEMO_MAP_ID",
-      });
-
-      // The marker
-      const marker = new AdvancedMarkerElement({
-        map: map,
-        position: position,
-        title: result[0].formatted_address,
-        content: mAptPin.element, // the marker is a pin
-        // content: beachFlagImg, // OR the marker is an icon
-      });
-    }
-
-    // Request needed libraries.
-    const geo = new Geocoder
-    geo.geocode({ address: this.addressValue }, set_map)
   }
-
 }
