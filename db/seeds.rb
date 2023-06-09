8e2ec7034aba04ca36abf7bc83140128f6fae3fe
Louis Rivest <louisrivest4@gmail.com>
2023-06-09 15:19:01 -0400

    last changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,15 +15,13 @@ user1 = User.create!(username: 'Smith', email: 'john@gmail.com', password: 'pass
 user2 = User.create!(username: 'Tim', email: 'tim@gmail.com', password: 'password')
 
 map1 = Map.create!(name: 'University', city: 'Montreal', user_id: user1.id)
-map2 = Map.create!(name: 'Summer Abroad', city: 'Athens', user_id: user1.id)
-map3 = Map.create!(name: 'Bachelor Party', city: 'Las Vegas', user_id: user2.id)
+map2 = Map.create!(name: 'Bachelor Party', city: 'Las Vegas', user_id: user2.id)
 
-PointOfInterest.create!(name: 'Tom', category: 'friend', address: '528 Victoria', latitude: '', longitude: '', map_id: map1.id)
-# PointOfInterest.create!(name: 'LeWagon', category: 'school', address: '5333 rue Casgrain #102, Montreal', latitude: 45.5261702, longitude: -73.5950124, map_id: map1.id)
-# PointOfInterest.create!(name: 'YMCA', category: 'gym', address: '528 Victoria', latitude: 45.5221592, longitude: -73.6215761, map_id: map1.id)
+PointOfInterest.create!(name: 'Tom', category: 'Friend/Family', address: '528 Victoria', latitude: '', longitude: '', map_id: map1.id)
+PointOfInterest.create!(name: 'Concordia', category: 'Education', address: '5333 rue Casgrain #102, Montreal', latitude: 45.5261702, longitude: -73.5950124, map_id: map1.id)
+PointOfInterest.create!(name: 'Mcdonalds', category: 'Restaurants', address: '528 Victoria', latitude: 45.5221592, longitude: -73.6215761, map_id: map1.id)
 
 
-PotentialLocation.create!(name: 'bassin', address: '560 Victoria', latitude: '', longitude: '', map_id: map1.id)
 PotentialLocation.create!(name: 'LeWagon', address: '5333 rue Casgrain #102, Montreal', latitude: 45.5261702, longitude: -73.5950124, map_id: map1.id)
 PotentialLocation.create!(name: 'YMCA', address: '528 Victoria', latitude: 45.5221592, longitude: -73.6215761, map_id: map1.id)
 
