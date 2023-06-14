b102265c56b82ffdc56d673dc58f9713d082218f
tainah jane <tainah.94@hotmail.com>
2023-06-14 17:43:18 -0400

    worked on buttons validations compare buttons

--- app/controllers/potential_locations_controller.rb
+++ app/controllers/potential_locations_controller.rb
@@ -3,6 +3,7 @@ class PotentialLocationsController < ApplicationController
   def create
     @map = Map.find(params[:map_id])
     @new_pot_loc = PotentialLocation.new(pot_locs_params)
+    @pot_locs = @map.potential_locations
     @new_pot_loc.map = @map
     if @new_pot_loc.save
       redirect_to map_path(@map), notice: "Potential Location was successfully created."
